2ccd50b0fd61d2eadc87aebca68d919c84e2ba21
Clara Beyer <csb324@gmail.com>
2016-11-14 11:46:53 -0500

    unlinking my unfinished portfolio from the footer. i should really finish that thing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default class Footer extends Component {
       <footer>
       	<p className="start-over"><button onClick={() => { this.handleRestart() } }>Start over</button></p>
 
-				<p className="credit"><a href="http://clarabeyer.co" target="_blank">clara fucking beyer</a> made this. she's on <a href="https://github.com/csb324" target="_blank">github</a> and <a href="https://twitter.com/clarabellum" target="_blank">twitter</a></p>
+				<p className="credit">clara fucking beyer started this. she's on <a href="https://github.com/csb324" target="_blank">github</a> and <a href="https://twitter.com/clarabellum" target="_blank">twitter</a></p>
         <p className="credit">many thanks to <a href="http://jezebel.com/a-list-of-pro-women-pro-immigrant-pro-earth-anti-big-1788752078">this article</a>, from which many of these links are taken. want to add something? submit a fucking <a href="https://github.com/csb324/holyfucktheelectionistomorrow" target="_blank">pull request</a></p>
 
       </footer>
